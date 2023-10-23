0e962d90ee995f480cafabefa27a682e74b446c7
JUNGFOCUS7 <jungfocus7@gmail.com>
2023-10-23 17:21:03 +0900

    ``
    ``

--- WebView2Tester__vb/GlobalSuppressions.vb
+++
@@ -1,7 +0,0 @@
-﻿
-' This file is used by Code Analysis to maintain SuppressMessage 
-' attributes that are applied to this project.
-' Project-level suppressions either have no target or are given 
-' a specific target and scoped to a namespace, type, member, etc.
-
-<Assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("스타일", "IDE1006:명명 스타일", Justification:="<보류 중>", Scope:="member", Target:="~M:WebView2Tester__vb.MainForm.pf_CoreWebView2InitializationCompleted(System.Object,Microsoft.Web.WebView2.Core.CoreWebView2InitializationCompletedEventArgs)")>
